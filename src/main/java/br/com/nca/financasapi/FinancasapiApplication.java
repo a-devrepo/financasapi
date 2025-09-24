251f54832d99a63e106737b41b49a205aff520e9
alison <adevpr11@gmail.com>
2025-09-24 11:37:53 -0300

    chore: estrutura inicial

---
+++ src/main/java/br/com/nca/financasapi/FinancasapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.nca.financasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancasapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancasapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/nca/financasapi/FinancasapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nca.financasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancasapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
