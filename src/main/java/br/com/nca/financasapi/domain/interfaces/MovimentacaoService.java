b890c4cf0087eeac3940ebf2aaa084c8ba367798
alison <adevpr11@gmail.com>
2025-09-29 11:40:44 -0300

    feat: define métodos de MovimentacaoService

--- src/main/java/br/com/nca/financasapi/domain/interfaces/MovimentacaoService.java
+++ src/main/java/br/com/nca/financasapi/domain/interfaces/MovimentacaoService.java
@@ -1,4 +1,22 @@
 package br.com.nca.financasapi.domain.interfaces;
 
+import br.com.nca.financasapi.domain.dtos.requests.AlterarMovimentacaoRequest;
+import br.com.nca.financasapi.domain.dtos.requests.CriarMovimentacaoRequest;
+import br.com.nca.financasapi.domain.dtos.responses.ConsultarMovimentacaoResponse;
+import org.springframework.data.domain.Page;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
 public interface MovimentacaoService {
+
+    ConsultarMovimentacaoResponse criar(CriarMovimentacaoRequest request);
+
+    ConsultarMovimentacaoResponse alterar(AlterarMovimentacaoRequest request);
+
+    ConsultarMovimentacaoResponse excluir(UUID id);
+
+    Page<ConsultarMovimentacaoResponse> consultar(LocalDate dataMin, LocalDate datamax, int page);
+
+    ConsultarMovimentacaoResponse obterPorId(UUID id);
 }
