22f5c2c2e5acd4f47d73e0a9cf31c122f6f85835
alison <adevpr11@gmail.com>
2025-10-01 15:34:05 -0300

    feat: implementa operações de crud para Movimentacoes

--- src/main/java/br/com/nca/financasapi/application/MovimentacoesController.java
+++ src/main/java/br/com/nca/financasapi/application/MovimentacoesController.java
@@ -2,11 +2,15 @@ package br.com.nca.financasapi.application;
 
 import br.com.nca.financasapi.domain.dtos.requests.AlterarMovimentacaoRequest;
 import br.com.nca.financasapi.domain.dtos.requests.CriarMovimentacaoRequest;
+import br.com.nca.financasapi.domain.dtos.responses.ConsultarMovimentacaoResponse;
 import br.com.nca.financasapi.domain.interfaces.MovimentacaoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -17,27 +21,32 @@ public class MovimentacoesController {
     private final MovimentacaoService movimentacaoService;
 
     @PostMapping
-    public ResponseEntity<?> post(@RequestBody CriarMovimentacaoRequest request) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<ConsultarMovimentacaoResponse> post(@RequestBody CriarMovimentacaoRequest request) {
+        var response = movimentacaoService.criar(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<?> put(@PathVariable UUID id, @RequestBody AlterarMovimentacaoRequest request) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<ConsultarMovimentacaoResponse> put(@RequestBody AlterarMovimentacaoRequest request) {
+        var response = movimentacaoService.alterar(request);
+        return ResponseEntity.ok().body(response);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable UUID id) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<ConsultarMovimentacaoResponse> delete(@PathVariable UUID id) {
+        var response = movimentacaoService.excluir(id);
+        return ResponseEntity.ok().body(response);
     }
 
-    @GetMapping("/{dataMin}/{dataMax}")
-    public ResponseEntity<?> get(@PathVariable String dataMin, @PathVariable String dataMax) {
-        return ResponseEntity.ok().build();
+    @GetMapping("/{dataMin}/{dataMax}/{page}")
+    public ResponseEntity<Page<ConsultarMovimentacaoResponse>> get(
+            @PathVariable String dataMin, @PathVariable String dataMax, @PathVariable int page) {
+        var resultado = movimentacaoService.consultar(LocalDate.parse(dataMin), LocalDate.parse(dataMax), page);
+        return ResponseEntity.ok().body(resultado);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> get(@PathVariable UUID id) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<ConsultarMovimentacaoResponse> get(@PathVariable UUID id) {
+        var response = movimentacaoService.obterPorId(id);
+        return ResponseEntity.ok().body(response);
     }
 }
--- src/main/java/br/com/nca/financasapi/infrastructure/repositories/MovimentacaoRepository.java
+++ src/main/java/br/com/nca/financasapi/infrastructure/repositories/MovimentacaoRepository.java
@@ -1,9 +1,22 @@
 package br.com.nca.financasapi.infrastructure.repositories;
 
 import br.com.nca.financasapi.domain.entities.Movimentacao;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, UUID> {
+
+    @Query("""
+            SELECT m FROM Movimentacao m
+            WHERE m.data BETWEEN :pDataMin AND :pDataMax ORDER BY m.data ASC
+            """)
+    Page<Movimentacao> findByDatas(
+            @Param("pDataMin") LocalDate dataMin,
+            @Param("pDataMax") LocalDate dataMax, Pageable pageable);
 }
