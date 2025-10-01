a60b5d39bab7b794b12be58d53552e50eb307e5c
alison <adevpr11@gmail.com>
2025-10-01 15:37:21 -0300

    feat: implementa operação para listar os tipos de movimentação

--- src/main/java/br/com/nca/financasapi/domain/services/TipoServiceImpl.java
+++ src/main/java/br/com/nca/financasapi/domain/services/TipoServiceImpl.java
@@ -2,14 +2,30 @@ package br.com.nca.financasapi.domain.services;
 
 import br.com.nca.financasapi.domain.dtos.responses.ConsultarTipoResponse;
 import br.com.nca.financasapi.domain.interfaces.TipoService;
+import br.com.nca.financasapi.infrastructure.repositories.TipoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@RequiredArgsConstructor
 @Service
 public class TipoServiceImpl implements TipoService {
+
+    private final TipoRepository tipoRepository;
+
     @Override
     public List<ConsultarTipoResponse> consultar() {
-        return List.of();
+
+        var lista = tipoRepository.findAll();
+
+        var resultado = new ArrayList<ConsultarTipoResponse>();
+
+        for (var tipo : lista) {
+            var dto = new ConsultarTipoResponse(tipo.getId(), tipo.getNome());
+            resultado.add(dto);
+        }
+        return resultado;
     }
 }
