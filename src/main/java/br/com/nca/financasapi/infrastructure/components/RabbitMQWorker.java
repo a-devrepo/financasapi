e159d65de516c658806d81a7ed7e6b2f70c5957c
alison <adevpr11@gmail.com>
2025-10-03 13:26:56 -0300

    feat: implementa mensageria e integração com openai

--- src/main/java/br/com/nca/financasapi/infrastructure/components/RabbitMQWorker.java
+++ src/main/java/br/com/nca/financasapi/infrastructure/components/RabbitMQWorker.java
@@ -1,23 +1,27 @@
 package br.com.nca.financasapi.infrastructure.components;
 
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
+@RequiredArgsConstructor
 @Component
 public class RabbitMQWorker {
 
     private static final Logger logger = LoggerFactory.getLogger(RabbitMQWorker.class);
 
+    private final OpenAIComponent openAIComponent;
+
     @RabbitListener(queues = "financas")
     public void receive(@Payload String message) {
-            logger.info("Mensagem lida da fila em [{}]: {}",
-                    LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")),
-                    message);
+        try{
+            var resposta = openAIComponent.send(message);
+            logger.info("Mensagem lida da fila: {}", resposta);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
